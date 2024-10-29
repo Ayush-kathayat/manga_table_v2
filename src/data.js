7a48acab1df70eb6462e7b1f5bd179e8124f7022
Ayush-kathayat <kathayatayush789@gmail.com>
2024-10-29 08:54:36 +0000

    feat: add one more status that is onhold and updated the status

--- src/data.js
+++ src/data.js
@@ -4,12 +4,13 @@ const STATUS_IN_PROGRESS = {
   name: "In Progress",
   color: "yellow.400",
 };
-const STATUS_NOT_STARTED = { id: 3, name: "Not Started", color: "pink.300" };
+const STATUS_ON_HOLD = { id: 3, name: "On Hold", color: "gray.300" };
+// const STATUS_NOT_STARTED = { id: 3, name: "Not Started", color: "pink.300" };
 const STATUS_FINISHED_READING = { id: 4, name: "Finished", color: "green.300" };
 export const STATUSES = [
   STATUS_ON_DROPPED,
   STATUS_IN_PROGRESS,
-  STATUS_NOT_STARTED,
+  STATUS_ON_HOLD,
   STATUS_FINISHED_READING,
 ];
 
@@ -34,19 +35,19 @@ const DATA = [
   },
   {
     link: "https://mangareader.to/spy-x-family-86",
-    status: null,
+    status: STATUS_IN_PROGRESS,
     due: new Date("2023/02/15"),
     chapters: 1,
   },
   {
     link: "https://mangareader.to/blue-lock-225",
-    status: STATUS_NOT_STARTED,
+    status: STATUS_IN_PROGRESS,
     due: null,
     chapters: 1,
   },
   {
     link: "https://mangareader.to/kaiju-no-8-1187",
-    status: null,
+    status: STATUS_IN_PROGRESS,
     due: new Date("2023/09/12"),
     chapters: 1,
   },
@@ -58,7 +59,7 @@ const DATA = [
   },
   {
     link: "https://mangareader.to/rentagirlfriend-2291",
-    status: STATUS_IN_PROGRESS,
+    status: STATUS_ON_DROPPED,
     due: null,
     chapters: 1,
   },
@@ -70,19 +71,19 @@ const DATA = [
   },
   {
     link: "https://mangareader.to/haikyu-15",
-    status: null,
+    status: STATUS_ON_HOLD,
     due: new Date("2023/09/30"),
     chapters: 1,
   },
   {
     link: "https://mangareader.to/a-couple-of-cuckoos-6376",
-    status: STATUS_IN_PROGRESS,
+    status: STATUS_ON_DROPPED,
     due: null,
     chapters: 1,
   },
   {
     link: "https://mangareader.to/komi-cant-communicate-287",
-    status: null,
+    status: STATUS_ON_DROPPED,
     due: new Date("2023/10/22"),
     chapters: 1,
   },
@@ -94,58 +95,46 @@ const DATA = [
   },
   {
     link: "https://mangareader.to/my-hero-academia-272",
-    status: STATUS_IN_PROGRESS,
-    due: null,
-    chapters: 1,
-  },
-  {
-    link: "https://mangareader.to/zom-100-bucket-list-of-the-dead-2235",
-    status: null,
-    due: new Date("2023/10/05"),
-    chapters: 1,
-  },
-  {
-    link: "https://mangareader.to/bungo-stray-dogs-156",
-    status: STATUS_ON_DROPPED,
-    due: new Date("2023/11/01"),
-    chapters: 1,
-  },
-  {
-    link: "https://mangareader.to/dr-stone-388",
     status: STATUS_FINISHED_READING,
     due: null,
     chapters: 1,
   },
   {
+    link: "https://mangareader.to/zom-100-bucket-list-of-the-dead-2235",
+    status: STATUS_ON_DROPPED,
+    due: new Date("2023/10/05"),
+    chapters: 1,
+  },
+  {
+    link: "https://mangareader.to/bungo-stray-dogs-156",
+    status: STATUS_ON_HOLD,
+    due: new Date("2023/11/01"),
+    chapters: 1,
+  },
+  {
+    link: "https://mangareader.to/dr-stone-388",
+    status: STATUS_ON_HOLD,
+    due: null,
+    chapters: 1,
+  },
+  {
     link: "https://mangareader.to/demon-slayer-kimetsu-no-yaiba-296",
-    status: null,
+    status: STATUS_FINISHED_READING,
     due: new Date("2023/09/25"),
     chapters: 1,
   },
   {
     link: "https://mangareader.to/solo-leveling-21",
-    status: STATUS_NOT_STARTED,
+    status: STATUS_FINISHED_READING,
     due: null,
     chapters: 1,
   },
   {
     link: "https://mangareader.to/tokyo-revengers-222",
-    status: null,
+    status: STATUS_FINISHED_READING,
     due: new Date("2023/10/10"),
     chapters: 1,
   },
-  // {
-  //   task: "Implement Mobile App Support",
-  //   status: STATUS_IN_PROGRESS,
-  //   due: null,
-  //   chapters: 1,
-  // },
-  // {
-  //   task: "Refine User Permission System",
-  //   status: null,
-  //   due: new Date("2023/09/18"),
-  //   chapters: 1,
-  // },
 ];
 
 export default DATA;
